6f4a53c6606e75e94efbd32ac7fc362ef6306c54
speed739 <56201854+speed739@users.noreply.github.com>
2019-10-05 19:35:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int odl = -1, suma = 0, i = 0;
+            float srd;
+
+            while(odl!=0)
+            {
+                string odp = Console.ReadLine();
+                odl = Convert.ToInt32(odp);
+
+                suma += odl;
+                i++;
+            }
+            srd = (float)suma / (i - 1);
+            Console.WriteLine("Srednia odleglosc - " + srd);
+        }
+    }
+}
