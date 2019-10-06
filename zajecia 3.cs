2a9b8b46813b6587dd8922310ba9adc2cbc8d810
speed739 <56201854+speed739@users.noreply.github.com>
2019-10-06 18:38:30 +0200

    Add files via upload

---
+++ zajecia 3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace zajecia_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder builder = new StringBuilder();
+            string text = string.Empty;
+            float suma = 0;
+            float wynik = 0;
+
+            while (text != "stop")
+            {
+                text = Console.ReadLine();
+                builder.Append(text + ";");
+            }
+
+            string[] oceny = builder.ToString().Split(new[] { "\r", "\n", "stop", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            float[] oce = new float[oceny.Length];
+
+            for (int i = 0; i < oce.Length; i++)
+            {
+                oce[i] = Convert.ToSingle(oceny[i]);
+            }
+
+            foreach (float ocena in oce)
+            {
+                suma += ocena;
+            }
+
+            wynik = suma / oce.Length;
+            Console.WriteLine(wynik.ToString("0.00"));
+
+            Console.ReadLine();
+        }
+    }
+}
